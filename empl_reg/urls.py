f2b2956d04316aa3a148bea80abdac541ebd7b05
Your Name <you@example.com>
2023-04-10 22:22:10 +0200

    before password reset

--- empl_reg/urls.py
+++ empl_reg/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path,include
 from . import views
 from django.contrib import admin
-
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
+    #reset password urls
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name = 'password_reset'),
+    path('password_reset/done', auth_views.PasswordResetDoneView.as_view(), name = 'password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name ='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name ='password_reset_complete'),
+    ##
     path('positions/', views.employee_pos, name = 'employee_pos'),
     path('admin/', admin.site.urls),
     path('accounts/', include("django.contrib.auth.urls")),
@@ -13,8 +19,5 @@ urlpatterns = [
     path('delete/<int:id>/', views.employee_delete, name = 'employee_delete'),
     path('list/', views.employee_list, name = 'employee_list')  #get request to retrieve and display all records
 ]
-
-
-
 
 
