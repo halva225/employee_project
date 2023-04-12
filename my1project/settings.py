de63572d9ac6a415a9d31982791b2192ffb3938b
Your Name <you@example.com>
2023-04-12 16:15:33 +0200

    2023.04.12/4:15

--- my1project/settings.py
+++ my1project/settings.py
@@ -131,3 +131,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = '/'
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'zenasaulak@gmail.com'
+EMAIL_HOST_PASSWORD = 'ozhe wqyh zavd yuop'
